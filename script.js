556b3718dfe7ee66cca6c53f9bce0777f5975748
lilferalguy <benaiahbm@gmail.com>
2023-07-23 04:44:38 -0400

    Finished function for getting computers choice in rock paper scissors

---
+++ script.js
@@ -0,0 +1,12 @@
+function getComputerChoice() {
+    let myArray = ["Rock", "Paper", "Scissors"];
+    let rand = myArray [Math.floor(Math.random() * myArray.length)];
+    console.log(rand);
+
+}
+
+getComputerChoice();
+
+function playRound (playerSelection, computerSelection) {
+    
+}
